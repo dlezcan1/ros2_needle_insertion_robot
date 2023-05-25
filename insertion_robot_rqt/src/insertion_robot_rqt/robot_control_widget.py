2a71d6d3b72b876a6c41660c0a8e59f4017172d0
Dimitri Lezcano <dlezcan1@jhu.edu>
2023-05-25 15:36:46 -0400

    Added X axis to UI

--- insertion_robot_rqt/src/insertion_robot_rqt/robot_control_widget.py
+++ insertion_robot_rqt/src/insertion_robot_rqt/robot_control_widget.py
@@ -38,84 +38,110 @@ class RobotControlWidget(QWidget):
         ui_file = os.path.join(package_path, 'share', 'insertion_robot_rqt', 'resource', 'RobotControl.ui')
         loadUi(ui_file, self)
 
-
-
         # "Initialize" and "State" functionality implementation
-        self.pushButton_toggleY.clicked.connect(lambda x: self.pushButton_srv_event("/stage/axis/state/toggle/y"))
-        self.pushButton_toggleZ.clicked.connect(lambda x: self.pushButton_srv_event("/stage/axis/state/toggle/z"))
-        self.pushButton_toggleLS.clicked.connect(lambda x: self.pushButton_srv_event("/stage/axis/state/toggle/linear_stage"))
+        self.pushButton_toggleY.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/state/toggle/x"))
+        self.pushButton_toggleY.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/state/toggle/y"))
+        self.pushButton_toggleZ.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/state/toggle/z"))
+        self.pushButton_toggleLS.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/state/toggle/linear_stage"))
         self.pushButton_toggleAll.clicked.connect(self.pushButton_toggleAll_event)
 
-        self.pushButton_zeroY.clicked.connect(lambda x: self.pushButton_srv_event("/stage/axis/zero/y"))
-        self.pushButton_zeroZ.clicked.connect(lambda x: self.pushButton_srv_event("/stage/axis/zero/z"))
-        self.pushButton_zeroLS.clicked.connect(lambda x: self.pushButton_srv_event("/stage/axis/zero/linear_stage"))
+        self.pushButton_zeroX.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/zero/x"))
+        self.pushButton_zeroY.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/zero/y"))
+        self.pushButton_zeroZ.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/zero/z"))
+        self.pushButton_zeroLS.clicked.connect(lambda: self.pushButton_srv_event("/stage/axis/zero/linear_stage"))
         self.pushButton_zeroAll.clicked.connect(self.pushButton_zeroAll_event)
 
 
+        self._node.create_subscription(Float32, "/stage/axis/position/x", lambda msg: self.axis_position_callback(msg, "x"), 10)
         self._node.create_subscription(Float32, "/stage/axis/position/y", lambda msg: self.axis_position_callback(msg, "y"), 10)
         self._node.create_subscription(Float32, "/stage/axis/position/z", lambda msg: self.axis_position_callback(msg, "z"), 10 )
         self._node.create_subscription(Float32, "/stage/axis/position/linear_stage", lambda msg: self.axis_position_callback(msg, "linear_stage"), 10 )
 
+        self._node.create_subscription(Bool, "/stage/axis/state/on/x", lambda msg: self.axis_state_callback(msg, "x"), 10)
         self._node.create_subscription(Bool, "/stage/axis/state/on/y", lambda msg: self.axis_state_callback(msg, "y"), 10)
         self._node.create_subscription(Bool, "/stage/axis/state/on/z", lambda msg: self.axis_state_callback(msg, "z"), 10)
         self._node.create_subscription(Bool, "/stage/axis/state/on/linear_stage", lambda msg: self.axis_state_callback(msg, "linear_stage"), 10)
 
         # "Move" functionality implementation
+        # - relative movements
+        self.label_relMoveX_min.setText(str(self.horizontalSlider_relMoveX.minimum()))
+        self.label_relMoveX_max.setText(str(self.horizontalSlider_relMoveX.maximum()))
+        
         self.label_relMoveY_min.setText(str(self.horizontalSlider_relMoveY.minimum()))
         self.label_relMoveY_max.setText(str(self.horizontalSlider_relMoveY.maximum()))
+        
         self.label_relMoveZ_min.setText(str(self.horizontalSlider_relMoveZ.minimum()))
         self.label_relMoveZ_max.setText(str(self.horizontalSlider_relMoveZ.maximum()))
+        
         self.label_relMoveLS_min.setText(str(self.horizontalSlider_relMoveLS.minimum()))
         self.label_relMoveLS_max.setText(str(self.horizontalSlider_relMoveLS.maximum()))
+        
+        # - absolute movements
+        self.label_absMoveX_min.setText(str(self.horizontalSlider_absMoveX.minimum()))
+        self.label_absMoveX_max.setText(str(self.horizontalSlider_absMoveX.maximum()))
+
         self.label_absMoveY_min.setText(str(self.horizontalSlider_absMoveY.minimum()))
         self.label_absMoveY_max.setText(str(self.horizontalSlider_absMoveY.maximum()))
+        
         self.label_absMoveZ_min.setText(str(self.horizontalSlider_absMoveZ.minimum()))
         self.label_absMoveZ_max.setText(str(self.horizontalSlider_absMoveZ.maximum()))
+        
         self.label_absMoveLS_min.setText(str(self.horizontalSlider_absMoveLS.minimum()))
         self.label_absMoveLS_max.setText(str(self.horizontalSlider_absMoveLS.maximum()))
 
+        self.lineedit_relMoveX.setInputMask("####")
         self.lineedit_relMoveY.setInputMask("####")
         self.lineedit_relMoveZ.setInputMask("####")
         self.lineedit_relMoveLS.setInputMask("####")
+        
+        self.lineedit_absMoveX.setInputMask("####")
         self.lineedit_absMoveY.setInputMask("####")
         self.lineedit_absMoveZ.setInputMask("####")
         self.lineedit_absMoveLS.setInputMask("####")
 
-        self.horizontalSlider_relMoveY.valueChanged.connect(lambda x: self.lineedit_move_event(self.horizontalSlider_relMoveY.value(), self.lineedit_relMoveY))
-        self.horizontalSlider_relMoveZ.valueChanged.connect(lambda x: self.lineedit_move_event(self.horizontalSlider_relMoveZ.value(), self.lineedit_relMoveZ))
-        self.horizontalSlider_relMoveLS.valueChanged.connect(lambda x: self.lineedit_move_event(self.horizontalSlider_relMoveLS.value(), self.lineedit_relMoveLS))
-        self.horizontalSlider_absMoveY.valueChanged.connect(lambda x: self.lineedit_move_event(self.horizontalSlider_absMoveY.value(), self.lineedit_absMoveY))
-        self.horizontalSlider_absMoveZ.valueChanged.connect(lambda x: self.lineedit_move_event(self.horizontalSlider_absMoveZ.value(), self.lineedit_absMoveZ))
-        self.horizontalSlider_absMoveLS.valueChanged.connect(lambda x: self.lineedit_move_event(self.horizontalSlider_absMoveLS.value(), self.lineedit_absMoveLS))
+        self.horizontalSlider_relMoveX.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_relMoveX.value(), self.lineedit_relMoveX))
+        self.horizontalSlider_relMoveY.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_relMoveY.value(), self.lineedit_relMoveY))
+        self.horizontalSlider_relMoveZ.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_relMoveZ.value(), self.lineedit_relMoveZ))
+        self.horizontalSlider_relMoveLS.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_relMoveLS.value(), self.lineedit_relMoveLS))
 
+        self.horizontalSlider_absMoveX.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_absMoveX.value(), self.lineedit_absMoveX))
+        self.horizontalSlider_absMoveY.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_absMoveY.value(), self.lineedit_absMoveY))
+        self.horizontalSlider_absMoveZ.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_absMoveZ.value(), self.lineedit_absMoveZ))
+        self.horizontalSlider_absMoveLS.valueChanged.connect(lambda: self.lineedit_move_event(self.horizontalSlider_absMoveLS.value(), self.lineedit_absMoveLS))
 
+        self.pushButton_relMoveX.clicked.connect(lambda: self.pushButton_move_event("x", isAbsolute=False))
+        self.pushButton_relMoveY.clicked.connect(lambda: self.pushButton_move_event("y", isAbsolute=False))
+        self.pushButton_relMoveZ.clicked.connect(lambda: self.pushButton_move_event("z", isAbsolute=False))
+        self.pushButton_relMoveLS.clicked.connect(lambda: self.pushButton_move_event("linear_stage", isAbsolute=False))
 
-        self.pushButton_relMoveY.clicked.connect(lambda x: self.pushButton_move_event("y", isAbsolute=False))
-        self.pushButton_relMoveZ.clicked.connect(lambda x: self.pushButton_move_event("z", isAbsolute=False))
-        self.pushButton_relMoveLS.clicked.connect(lambda x: self.pushButton_move_event("linear_stage", isAbsolute=False))
+        self.pushButton_absMoveX.clicked.connect(lambda: self.pushButton_move_event("x", isAbsolute=True))
+        self.pushButton_absMoveY.clicked.connect(lambda: self.pushButton_move_event("y", isAbsolute=True))
+        self.pushButton_absMoveZ.clicked.connect(lambda: self.pushButton_move_event("z", isAbsolute=True))
+        self.pushButton_absMoveLS.clicked.connect(lambda: self.pushButton_move_event("linear_stage", isAbsolute=True))
 
-        self.pushButton_absMoveY.clicked.connect(lambda x: self.pushButton_move_event("y", isAbsolute=True))
-        self.pushButton_absMoveZ.clicked.connect(lambda x: self.pushButton_move_event("z", isAbsolute=True))
-        self.pushButton_absMoveLS.clicked.connect(lambda x: self.pushButton_move_event("linear_stage", isAbsolute=True))
-
-        self.pushButton_abort.clicked.connect(lambda x: self.pushButton_srv_event("/stage/abort"))
+        self.pushButton_abort.clicked.connect(lambda: self.pushButton_srv_event("/stage/abort"))
 
     def axis_position_callback(self, msg, axisName):
-        if (axisName=="y"):
+        if (axisName == "x"):
+            self.lcdNumber_X.display(msg.data)
+        if (axisName == "y"):
             self.lcdNumber_Y.display(msg.data)
-        if (axisName=="z"):
+        if (axisName == "z"):
             self.lcdNumber_Z.display(msg.data)
-        if (axisName=="linear_stage"):
+        if (axisName == "linear_stage"):
             self.lcdNumber_LS.display(msg.data)
 
     def axis_state_callback(self, msg, axisName):
-        if (axisName=="y"):
+        if (axisName == "x"):
+            self.label_stateX.setText("ON" if msg.data else "OFF")
+            self.label_stateX.setStyleSheet("background-color: green; border: 1px solid black;" if msg.data else "background-color: red; border: 1px solid black;")
+        if (axisName == "y"):
             self.label_stateY.setText("ON" if msg.data else "OFF")
             self.label_stateY.setStyleSheet("background-color: green; border: 1px solid black;" if msg.data else "background-color: red; border: 1px solid black;")
-        if (axisName=="z"):
+        if (axisName == "z"):
             self.label_stateZ.setText("ON" if msg.data else "OFF")
             self.label_stateZ.setStyleSheet("background-color: green; border: 1px solid black;" if msg.data else "background-color: red; border: 1px solid black;")
-        if (axisName=="linear_stage"):
+        if (axisName == "linear_stage"):
             self.label_stateLS.setText("ON" if msg.data else "OFF")
             self.label_stateLS.setStyleSheet("background-color: green; border: 1px solid black;" if msg.data else "background-color: red; border: 1px solid black;")
 
@@ -141,21 +167,18 @@ class RobotControlWidget(QWidget):
         self._node.destroy_client(cli)
 
     def pushButton_toggleAll_event(self):
+        self.pushButton_srv_event("/stage/axis/state/toggle/x")
         self.pushButton_srv_event("/stage/axis/state/toggle/y")
         self.pushButton_srv_event("/stage/axis/state/toggle/z")
         self.pushButton_srv_event("/stage/axis/state/toggle/linear_stage")
 
     def pushButton_zeroAll_event(self):
+        self.pushButton_srv_event("/stage/axis/zero/x")
         self.pushButton_srv_event("/stage/axis/zero/y")
         self.pushButton_srv_event("/stage/axis/zero/z")
         self.pushButton_srv_event("/stage/axis/zero/linear_stage")
 
-
-
-
-
-    def pushButton_move_event(self, axisName, isAbsolute="false"):
-        
+    def pushButton_move_event(self, axisName, isAbsolute=False):
         topicName="stage/axis/command/"
         if (isAbsolute):
             topicName+="absolute/"
@@ -166,22 +189,37 @@ class RobotControlWidget(QWidget):
             Float32, topicName + axisName, qos_profile=QoSProfile(depth=10))
         msg = Float32()
         
-        if (axisName=="y"):
+        if (axisName == "x"):
             if (isAbsolute):
-                lineEdit=self.lineedit_absMoveY
+                slider   = self.horizontalSlider_absMoveX
+                lineEdit = self.lineedit_absMoveX
             else:
-                lineEdit=self.lineedit_relMoveY
-        elif (axisName=="z"):
+                slider   = self.horizontalSlider_relMoveX
+                lineEdit = self.lineedit_relMoveX
+        elif (axisName == "y"):
             if (isAbsolute):
-                lineEdit=self.lineedit_absMoveZ
+                slider   = self.horizontalSlider_absMoveY
+                lineEdit = self.lineedit_absMoveY
             else:
-                lineEdit=self.lineedit_relMoveZ
-        elif (axisName=="linear_stage"):
+                slider   = self.horizontalSlider_relMoveY
+                lineEdit = self.lineedit_relMoveY
+        elif (axisName == "z"):
             if (isAbsolute):
-                lineEdit=self.lineedit_absMoveLS
+                slider   = self.horizontalSlider_absMoveZ
+                lineEdit = self.lineedit_absMoveZ
             else:
-                lineEdit=self.lineedit_relMoveLS
+                slider   = self.horizontalSlider_relMoveZ
+                lineEdit = self.lineedit_relMoveZ
+        elif (axisName == "linear_stage"):
+            if (isAbsolute):
+                slider   = self.horizontalSlider_absMoveLS
+                lineEdit = self.lineedit_absMoveLS
+            else:
+                slider   = self.horizontalSlider_relMoveLS
+                lineEdit = self.lineedit_relMoveLS
         
         
         msg.data = float(lineEdit.text())
+        slider.setValue(int(msg.data))
         pub.publish(msg)
+
